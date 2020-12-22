167d863b498fdb784458276f0a0687ebbd40b8ab
Toru Ochiai <toru@indeed.com>
2020-12-22 07:15:06 +0000

    Add basic linux related files

---
+++ linux-work/env.sh
@@ -0,0 +1,6 @@
+export EDITOR=/usr/bin/vi
+
+parse_git_branch() {
+    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1) /'
+}
+export PS1="\h: \w \[\033[33m\]\$(parse_git_branch)\[\033[00m\]\\$\[$(tput sgr0)\] "
