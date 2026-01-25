8dc18cd12592e310a64114e882d68cf3f9773797
Otchy <id+github@otchy.net>
2026-01-25 13:28:55 +0900

    Update ffmpeg.sh

--- mac-common/ffmpeg.sh
+++ mac-common/ffmpeg.sh
@@ -19,8 +19,29 @@ function conv-movie-small-gif(){
     ffmpeg -i "$1" -filter_complex "[0:v] fps=15,scale=640:-1,split [a][b];[a] palettegen [p];[b][p] paletteuse" "$2"
 }
 
+function conv-movie-smooth-gif(){
+    #ffmpeg -i $1 -vf scale=640:-1 -r 15 $2
+    ffmpeg -i "$1" -filter_complex "[0:v] fps=30,scale=640:-1,split [a][b];[a] palettegen [p];[b][p] paletteuse" "$2"
+}
+
 function concat-movies(){
     ffmpeg -safe 0 -f concat -i $1 -c copy $2
+}
+
+function cut-movie(){
+    if [ "$#" -eq 0 ]; then
+        echo "Usage: cut-movie mm:ss input.mp4"
+        return 1
+    fi
+    ffmpeg -ss $1 -i "$2" -c copy "${2}_cut.mp4"
+}
+
+function cut-movie-last(){
+    if [ "$#" -eq 0 ]; then
+        echo "Usage: cut-movie-last mm:ss input.mp4"
+        return 1
+    fi
+    ffmpeg -i ${2} -t ${1} -c copy "${2}_cut.mp4"
 }
 
 export PATH=$DOT_ROOT/mac-common/ffmpeg-bin:$PATH
